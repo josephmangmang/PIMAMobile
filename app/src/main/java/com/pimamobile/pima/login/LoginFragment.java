182e6971e3396cc8bd2fb19a8a031fd7ee1f3f82
kali_root <josephmangmang@gmail.com>
2016-02-08 23:44:54 +0800

    Added Login Logic

--- app/src/main/java/com/pimamobile/pima/login/LoginFragment.java
+++ app/src/main/java/com/pimamobile/pima/login/LoginFragment.java
@@ -31,14 +31,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.pimamobile.pima.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
 public class LoginFragment extends Fragment {
+    private static final String SERVE_URL = "http://www.pretextsms.com/pima/api.php";
     private EditText mUserName;
     private EditText mUserPassword;
     private Button mLoginButton;
@@ -84,6 +93,24 @@ public class LoginFragment extends Fragment {
 
     private void loginUser() {
         boolean rememberMe = mRememberMe.isChecked();
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, SERVE_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
 
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+
+                return params;
+            }
+        };
     }
 }
